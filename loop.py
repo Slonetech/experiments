f0826c618954f4c65876f798257013d6a901cc6f
Slonetech <kengeregordon@gmail.com>
2023-11-30 13:07:02 +0300

    step

--- loop.py
+++ loop.py
@@ -3,3 +3,8 @@ for i in range (1, 5):
     for j in range (i):
         print("*", end = "")
     print()
+
+
+    #the range function can also accept one more argument which is the step size
+    #range(start, stop, step)
+    #step size is the increment size .
